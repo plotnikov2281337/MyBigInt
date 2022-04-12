3d8d7d0faf497d86ba20afb6deb5154a7571260a
user <user@C1012>
2022-04-12 11:47:36 +0300

    Добавьте файлы проекта.

---
+++ MyBigInt/BigInt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBigInt
+{
+    internal class BigInt
+    {
+
+        public void Insert(List<int> list)
+        {
+            Console.WriteLine("Введите число: ");
+            string _value = Console.ReadLine();
+            for (int i = 0; i < _value.Length; i++)
+            {
+                string s = _value[i].ToString();
+                list.Add(int.Parse(s));
+            }
+            Print(list);
+            Console.ReadKey();
+        }
+
+
+
+        public void Print(List<int> someList)
+        {
+            foreach (var item in someList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ MyBigInt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBigInt
+{
+    internal class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            var num1 = new List<int>();
+            var num2 = new List<int>();
+            BigInt bigInt = new BigInt();
+
+            bigInt.Insert(num1);
+            bigInt.Insert(num2);
+        }
+    }
+}
